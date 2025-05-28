40aca03a1fefc0040a17a519dbfa121ae9f5599c
KevinJHP19 <kevinjordanhuaman@gmail.com>
2025-05-28 18:37:33 +0200

    ahora se puede desplegar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,9 @@ import Pricing from './Pricing/page';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black-900">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-      <p className="mt-4 text-lg">This is a simple Next.js application.</p>
-      <div className="mt-8">
-        <a
-          href="https://nextjs.org/docs"
-          className="text-blue-600 hover:underline"
-        >
-          Learn more about Next.js
-        </a>
-      </div>
+      <Docs />
+      <Pricing />
+      
     </main>
     
   );
