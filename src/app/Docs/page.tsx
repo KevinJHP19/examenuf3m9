d1342cef392b2f65d0c6ee9f360c43ee80a714e4
KevinJHP19 <kevinjordanhuaman@gmail.com>
2025-05-28 18:58:29 +0200

    docs sin  responsive hecho

--- src/app/Docs/page.tsx
+++ src/app/Docs/page.tsx
@@ -1,7 +1,30 @@
+import { Button } from "@/components/ui/button";
 export default function Docs() {
-
     return (
-        <h1>Docs</h1>
-    )
+        <section className="flex flex-col items-center justify-center bg-black-900 p-6">
+            <div className="info w-full max-w-[642px]">
+                <h1 className="text-3xl font-bold text-white sm:text-4xl text-center text-[48px]">
+                    Information you need during on-call emergencies
+                </h1>
+                <p className="mt-6 text-lg text-gray-300 text-center text-light text-[18px]">
+                    Quickly link new on-call tickets to similar past incidents and their solutions. All directly in Slack the moment an incident happens.
+                </p>
+            </div>
 
+            <div className="mt-8 flex justify-center">
+                <Button className="bg-white text-black px-6 py-3 rounded-md font-semibold text-[14px]">
+                    Get Started {">"}
+                </Button>
+            </div>
+
+            <div className="image w-full flex justify-center">
+                <img
+                    src="portada.png"
+                    alt="portada"
+                    className="mt-8 w-full   rounded-lg shadow-lg w-[1024px] h-[632.53px] rounded-[4px]
+]"
+                />
+            </div>
+        </section>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,9 @@ import Pricing from './Pricing/page';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black-900">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-black-900 w-[1280px] mx-auto">
       <Docs />
       <Pricing />
-      
     </main>
     
   );
